0ef5fb887a368adebeb5d509284f01545d726a2b
Carl Upton <carl@schoolcomms.co.uk>
2018-09-16 17:44:30 +0100

    adding cors business

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ class App {
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false}));
+        this.app.options('*', cors()) 
         this.app.use(cors());
     }
 
